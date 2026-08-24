ec6d9230de8350c3ddef339ee96756bbc0b06083
Manuuhbs <manuela.carlesso@universo.univates.br>
2026-08-24 18:36:46 -0300

    fotos mexendo

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,6 +25,6 @@ botoes.forEach((botao) => {
 });
 botaoSecreto.addEventListener("click", () => {
   alert(
-    "Você: Eu amo muito você, seu corpo, seus braços, seu rosto, seu humor, personalidade, gostos, tudo que faz você ser esse alguém tão importante para mim, a gente nem sempre é perfeito, mas o que importa é no fim do dia, poder contar um com o outro e estar sempre presente. Eu te amo com todo o meu ser e continuarei amando enquanto estiver respirando, Feliz 21 meu amor❤️",
+    "Você: Eu amo muito você, seu corpo, seus braços, seu rosto, seu humor, personalidade, gostos, tudo que faz você ser esse alguém tão importante para mim. Você é a primeira coisa que penso quando acordo e a última ao dormir. A gente nem sempre é perfeito, mas o importante é no fim do dia, poder contar um com o outro e estar sempre presente, amando um ao outro acima de tudo, se apoiando e rindo igual dois idiotas. Meu garoto, meu homem, meu namorado, meu melhor amigo, minha pessoa favorita, eu te amo com todo o meu ser e continuarei amando enquanto estiver respirando. Feliz 21 meu amor❤️",
   );
 });
